3cd87125da97b2515d01f79897a0e37b0fad7d4c
txsniper <txawplh@gmail.com>
2018-03-13 19:04:25 +0800

    fix bug

--- Handson-ML/chapter5.py
+++ Handson-ML/chapter5.py
@@ -64,7 +64,7 @@ class SVM(object):
         from sklearn.svm import SVC
         poly_kernel_svm_clf = Pipeline((
             ('scaler', StandardScaler()),
-            ('svm_clf', SVC(kernel='poly', degree=3, ceof=0.1, C=5))
+            ('svm_clf', SVC(kernel='poly', degree=3, coef0=1, C=5))
         ))
         poly_kernel_svm_clf.fit(X, y)
         poly100_kernel_svm_clf = Pipeline([

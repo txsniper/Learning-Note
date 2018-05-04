75f6c1ff657aabe7453395881cad6a133b8e3107
txsniper <txawplh@gmail.com>
2018-05-04 14:32:55 +0800

    update

--- gluon-mxnet/深度学习模型基础/ch3.py
+++ gluon-mxnet/深度学习模型基础/ch3.py
@@ -25,5 +25,58 @@ def get_text_labels(label):
     return [text_labels[int(i)] for i in label]
 
 data, label = mnist_train[0:9]
-show_images(data)
-print(get_text_labels(label))
+#show_images(data)
+#print(get_text_labels(label))
+
+# step1: 划分数据集
+batch_size = 256
+train_data = gluon.data.DataLoader(mnist_train, batch_size, shuffle=True)
+test_data = gluon.data.DataLoader(mnist_test, batch_size, shuffle=False)
+
+# step2: 初始化模型参数
+num_inputs = 784
+num_output = 10
+W = nd.random_normal(shape=(num_inputs, num_output))
+b = nd.random_normal(shape=num_output)
+params = [W, b]
+for param in params:
+    param.attach_grad()
+
+# step3: 定义模型
+from mxnet import nd
+def softmax(X):
+    exp = nd.exp(X)
+    partition = exp.sum(axis=1, keepdims=True)
+    return exp / partition
+
+def net(X):
+    return softmax(nd.dot(X.reshape((-1, num_inputs)), W) + b)
+
+def cross_entropy(yhat, y):
+    return -nd.pick(nd.log(yhat), y)
+
+import sys
+sys.path.append('..')
+from utils import *
+from mxnet import autograd
+
+learning_rate = 0.1
+for epoch in range(5):
+    train_loss = 0
+    train_acc = 0
+    for data, label in train_data:
+        with autograd.record():
+            output = net(data)
+            loss = cross_entropy(output, label)
+        loss.backward()
+        SGD(params, learning_rate / batch_size)
+
+        train_loss += nd.mean(loss).asscalar()
+        train_acc += accuracy(output, label)
+    
+    test_acc = evaluate_accuracy(test_data, net)
+    print("Epoch %d. Loss: %f, Train acc %f, Test acc %f" % (
+        epoch, train_loss/len(train_data), train_acc/len(train_data), test_acc
+    ))
+
+        

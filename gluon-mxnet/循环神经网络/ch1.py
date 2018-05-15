b855f568629baa758a94fd97817f23f157a3da1b
txsniper <txawplh@gmail.com>
2018-05-15 20:01:28 +0800

    update

--- gluon-mxnet/循环神经网络/ch1.py
+++ gluon-mxnet/循环神经网络/ch1.py
@@ -7,14 +7,14 @@ with open('./data/jaychou_lyrics.txt') as f:
 #print(corpus_chars[0:49])
 
 corpus_chars = corpus_chars.replace('\n', ' ').replace('\r', ' ')
-corpus_chars = corpus_chars[0:20000]
+corpus_chars = corpus_chars[0:60]
 idx_to_char = list(set(corpus_chars))
 char_to_idx = dict([(char, i) for i, char in enumerate(idx_to_char)])
 vocab_size = len(char_to_idx)
 print('vocab size:', vocab_size)
 
 corpus_indices = [char_to_idx[char] for char in corpus_chars]
-sample = corpus_indices[:40]
+sample = corpus_indices[:5]
 print('chars:\n', ''.join([idx_to_char[idx] for idx in sample]))
 print('\nindices:\n', sample)
 
@@ -24,7 +24,10 @@ from mxnet import nd
 # 随机批量采样
 def data_iter_random(corpus_indices, batch_size, num_steps, ctx=None):
     # 总样本数量
-    num_examples = (len(corpus_indices) - 1)
+    num_examples = (len(corpus_indices) - 1) // num_steps
+    print(len(corpus_indices))
+    print(num_steps)
+    print(num_examples)
     epoch_size = num_examples // batch_size
 
     # 下标随机
@@ -45,8 +48,8 @@ def data_iter_random(corpus_indices, batch_size, num_steps, ctx=None):
         )
         yield data, label
 
-my_seq = list(range(30))
-for data, label in data_iter_random(my_seq, batch_size=2, num_steps=3):
+my_seq = list(range(5))
+for data, label in data_iter_random(my_seq, batch_size=2, num_steps=2):
     print('data: ', data, '\nlabel:', label, '\n')
 
 # 相邻批量采样
@@ -68,3 +71,47 @@ my_seq = list(range(30))
 for data, label in data_iter_consecutive(my_seq, batch_size=2, num_steps=3):
     print('data: ', data, '\nlabel:', label, '\n')
 
+
+def get_inputs(data):
+    return [nd.one_hot(X, vocab_size) for X in data.T]
+
+print(data)
+inputs = get_inputs(data)
+print('input length:', len(inputs))
+print('input shape: ', inputs)
+print('input shape: ', inputs[0])
+
+
+import mxnet as mx
+import sys 
+sys.path.append('..')
+import gluonbook as gb
+ctx = gb.try_gpu()
+print('Will use', ctx)
+input_dim = vocab_size
+hidden_dim = 256
+output_dim = vocab_size
+std = 0.01
+
+def get_params():
+    W_xh = nd.random_normal(scale=std, shape=(input_dim, hidden_dim), ctx=ctx)
+    W_hh = nd.random_normal(scale=std, shape=(hidden_dim, hidden_dim), ctx=ctx)
+    b_h = nd.zeros(hidden_dim, ctx=ctx)
+
+    W_hy = nd.random_normal(scale=std, shape=(hidden_dim, output_dim), ctx=ctx)
+    b_y  = nd.zeros(output_dim, ctx=ctx)
+
+    params = [W_xh, W_hh, b_h, W_hy, b_y]
+    for param in params:
+        param.attach_grad()
+    return params
+
+def rnn(inputs, state, *params):
+    H = state
+    W_xh, W_hh, b_h, W_hy, b_y = params
+    outputs = []
+    for X in inputs:
+        H = nd.tanh(nd.dot(X, W_xh) + nd.dot(H, W_hh) + b_h)
+        Y = nd.dot(H, W_hy) + b_y
+        outputs.append(Y)
+    return (outputs, H)

3b8cde84260ed31ef2293f5b3cd9bcae84f05dcf
txsniper <txawplh@126.com>
2018-06-08 09:49:52 +0800

    update

---
+++ Handson-ML/kaggle/avazu_ctr_pred/main.py
@@ -0,0 +1,36 @@
+import pandas as pd 
+import numpy as np 
+
+
+class Solution(object):
+    def __init__(self, data_path):
+        self.data_path = data_path
+        self.train_csv = data_path + "/train/train.csv"
+        self.test_csv = data_path + "/test/test.csv"
+        self.all_features = ['click', 'hour', 'C1', 'banner_pos', 'site_id', 'site_domain', 'site_category', 'app_id', 'app_domain', 'app_category', 'device_id', 'device_ip', 'device_model', 'device_type', 'device_conn_type', 'C14', 'C15', 'C16', 'C17', 'C18', 'C19', 'C20', 'C21']
+        
+
+    def display_feature(self, data_frame, feature_name):
+        print("----------" + feature_name + "------------")
+        ret = data_frame[feature_name].count()
+        print(ret)
+        ret1 = data_frame[feature_name].value_counts().nlargest(20)
+        print(ret1)
+
+    def load_data(self):
+        self.train_data = pd.read_csv(self.train_csv, nrows=100000)
+
+        #print(self.train_data.columns.values.tolist())
+        #print(self.train_data.describe())
+        #print(self.train_data.values_count())
+        for feature in self.all_features:
+            self.display_feature(self.train_data, feature)
+
+
+def main():
+    path = "D:\\BaiduYunDownload\\avazu-ctr-prediction"
+    solution = Solution(path)
+    solution.load_data()
+
+if __name__ == "__main__":
+    main()

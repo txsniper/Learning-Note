09018a3bc1167b05a41eccd144dd8e5e4d08df0a
xing.tan_ele.me_waimai <txawplh@gmail.com>
2018-06-19 20:30:09 +0800

    update

--- Handson-ML/kaggle/avazu_ctr_pred/my solution/3rd/script/addc.py
+++ Handson-ML/kaggle/avazu_ctr_pred/my solution/3rd/script/addc.py
@@ -35,24 +35,57 @@ class Addc(object):
         for key, value in self.col_idx:
             self.idx_col[value] = key
 
+        self.feature_prefix = {
+            "site_id":    "c",
+            'app_id' :    "f",
+            'device_id' : "i",
+            "device_ip" : "j",
+            "device_model" : "k",
+            'device_type' : "l",
+            'device_conn_type' : "m",
+            'C14' : "n",
+            'C15' : "o",
+            'C16' : "p",
+            'C17' : "q",
+            'C18' : "r",
+            'C19' : "s",
+            'C20' : "t",
+            'C21' : "u",  
+        }
 
+    # 利用device_id来标识用户，如果device_id是"a99f214a"，
+    # 则使用 dev_ip + dev_model 组合
     def get_id(self, dev_id, dev_ip, dev_model):
         if dev_id != 'a99f214a':
             return dev_id
         else:
             return dev_ip + "_" + dev_model
 
+
     def process_data(self, input, output, isTest):
         f_out = open(output, "w")
+        
         cols_name = []
+        line_count = 0
+        feature_dict = defaultdict(int)
+        feature_dict_1 = defaultdict(int)
         with open(input) as f_in:
             for line in f_in:
                 line = line.strip()
+                if len(line) == 0:
+                    break
+                line_count += 1
+                if line_count == 1:
+                    f_out.write(line[:-2] + ",C22,C23,C24,C25,C26,C27,C28\n")
+                if line_count % 100000 == 0:
+                    print("line count : " + str(line_count))
                 parts = line.split(",")
                 if len(cols_name) == 0:
                     cols_name = parts
                     continue
                 id = parts[0]
+
+                # 训练数据和测试数据的 date_idx 不一样
                 date_idx = 2
                 if isTest is True:
                     date_idx = 1
@@ -86,11 +119,38 @@ class Addc(object):
                 day   = date[4:6]
                 hour = date[6:]
 
-                id = self.get_id(device_id, device_ip, device_model)
+                id = self.get_id("i#" + device_id, "j#" + device_ip, "k#" + device_model)
+                id_C14 = id + "_" + "n#" + C14
+                id_C17 = id + "_" + "q#" + C17
+                feature_dict[id_C14] += 1
+                feature_dict[id_C17] += 1
 
+                feature_dict_1[id_C14] += 1
+                feature_dict_1[id_C17] += 1
+                feature_dict_1[id] += 1
 
-
+                median_id = "f#" + app_id
+                if app_id == "ecad2386":
+                    median_id = "c#" + site_id
                 
+                feature_dict[id + "_" + median_id] += 1
+
+                m  = feature_dict[id + "_" + median_id]
+                c  = feature_dict[id_C14]
+                c2 = feature_dict[id_C17]
+
+                ch = feature_dict_1[id_C14]
+                ch1 = feature_dict_1[id_C17]
+                ch2 = feature_dict_1[id]
+
+
+
+
+
+
+
+
+
 
 
 

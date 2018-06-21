37f4f62137e7eaa2c296f81953bfe9ff7b94ec2b
xing.tan_ele.me_waimai <txawplh@gmail.com>
2018-06-21 20:06:46 +0800

    update

---
+++ Handson-ML/kaggle/avazu_ctr_pred/my solution/3rd/script/6_id_stat.py
@@ -0,0 +1,51 @@
+import marshal
+class IdStat(object):
+    def __init__(self):
+        pass
+    
+    def id_count(self, dic, parts, idx, prefix):
+        id = prefix + parts[idx]
+        if id in dic:
+            dic[id] += 1
+        else:
+            dic[id] = 1
+        return dic
+
+    def process(self, file_in, dic, isTest):
+        f_in = open(file_in)
+        line = f_in.readline()
+        count = 1
+        while True:
+            line = f_in.readline()
+            line = line.strip()
+            if not line:
+                break
+            count += 1
+            if count % 100000 == 0:
+                print("line count : " + str(count) + "\n")
+            parts = line.split(",")
+            # id
+            id_idx = len(parts) - 7
+            dic = self.id_count(dic, parts, id_idx, "v#")
+            # device_ip
+            ip_idx = len(parts) - 19
+            dic = self.id_count(dic, parts, ip_idx, "j#")
+            # device_id
+            did_idx = len(parts) - 20
+            dic = self.id_count(dic, parts, did_idx, "i#")
+        f_in.close()
+        return dic
+
+              
+    def run(self, train_in, test_in, file_out):
+        dic = {}
+        dic = self.process(train_in, dic, False)
+        dic = self.process(test_in, dic, True)
+        marshal.dump(dic, open(file_out, "w"))
+
+if __name__ == "__main__":
+    train_in = "./data_out/train_c"
+    test_in  = "./data_out/test_c"
+    file_out = "./data_out/id_stat"
+    obj = IdStat()
+    obj.run(train_in, test_in, file_out)
